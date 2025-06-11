df44ada84d8bc33500c034582313bbb691eb1fdf
jingqing-win11 <tianwei.jiang@qq.com>
2025-06-11 15:32:12 +0100

    项目成功初始化，可运行

---
+++ src/main/java/com/graysea/GrayseaTravelApplication.java
@@ -0,0 +1,11 @@
+package com.graysea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrayseaTravelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrayseaTravelApplication.class, args);
+    }
+}
